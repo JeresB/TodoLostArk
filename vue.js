2deef955b0f53d1f497cb4a15275a81f2bd8346b
Jeremy <boiselet.jeremy@gmail.com>
2022-06-03 15:43:50 +0200

    Add files via upload

--- vue.js
+++ vue.js
@@ -4,42 +4,172 @@ const db = new StormDB(engine);
 var persoEnCours = [];
 
 $(document).ready(function () {
-    
+    showPersos();
+    showEvents();
+    showRapport();
+    showImportantFromOtherPerso();
+    showCounter();
+});
+
+$(document).on('click', '.cardEvent', function () {
+    checkTask($(this).data('id'));
+    $(this).hide();
+});
+
+function showPersos() {
+    let htmlPersos = '';
+
     getPersoFromGroupe('Main');
     getPersoFromGroupe(db.get("groupeEnCours").value());
 
     persoEnCours.forEach(function (p) {
         let htmlTaches = '';
+        let htmlEntete = '';
+        let tasks = getTachesActiveFromPerso(p);
 
-        getTachesFromPerso(p).forEach(function (t) {
+        tasks.sort((a, b) => {
+            return a.prioTask - b.prioTask;
+        });
+
+        tasks.forEach(function (t) {
             let i = getIndexTask(t);
+            let color = getColorFromTask(t);
 
             htmlTaches += `
-            <div class="card mb-3">
+            <div class="card mb-3 cardEvent" data-id="${i}" style="cursor: pointer;">
                 <div class="d-flex">
-                    <div class="card-body">
-                        ${t.nomTask}<br>
-                        <i class="color-gray">${t.typeTask} - ${t.dureeTask} min - ${t.persoTask}</i>
-                        <div class="form-check form-switch float-end">
-                            <input class="form-check-input switchMajTask" data-index="${i}" data-champs="statutTask" type="checkbox" id="statutTaskDetail${i}" ${t.statutTask ? 'checked' : ''}>
-                        </div>
+                    <div class="card-body ${color}">
+                        ${t.nomTask}
                     </div>
                 </div>
             </div>`;
         });
 
-        let html = `
-            <section style="background-color: red;width: 20vw;height: 100vh;">
-                <img src="images/${p.imagePerso}" alt="" style="width: 18vw;">
+        if (p.groupePerso == 'Main') {
+            htmlEntete = `
+                <div class="card mb-3">
+                    <div class="d-flex">
+                        <div class="card-body">
+                            Perso -> <strong><i>MAIN</i></strong>
+                        </div>
+                    </div>
+                </div>
+            `;
+        } else {
+            htmlEntete = `
+                <div class="card mb-3">
+                    <div class="d-flex">
+                        <div class="card-body">
+                            Perso groupe -> <strong><i>${p.groupePerso}</i></strong>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
 
-                <section id="main-taches" style="width: 18vw;background-color: blue;">
+        htmlPersos += `
+            <section style="width: 18vw;">
+                ${htmlEntete}
+                <img src="images/${p.imagePerso}" alt="" style="width: 100%;height: 38vh;margin-bottom: 20px;border-radius: 5px;">
+
+                <section id="main-taches" style="width: 100%">
                     ${htmlTaches}
                 </section>
             </section>`;
-
-        $('#sectionPersoEnCours').html(html);
     });
-});
+
+    $('#sectionPersoEnCours').html(htmlPersos);
+}
+
+function showEvents() {
+    let eventTasks = getEventTask();
+    let eventTasksDaily = [];
+    let htmlTachesEvent = ``;
+
+    eventTasks.forEach(function (task) {
+        db.get("times").value().forEach(function (time) {
+            if (task.openingTask == time.typeEvent) {
+                if (moment().isoWeekday() == time.day && !task.statutTask) {
+                    eventTasksDaily.push(task);
+                }
+            }
+        });
+    });
+
+    eventTasksDaily.forEach(function (t) {
+        let i = getIndexTask(t);
+
+        htmlTachesEvent += `
+        <div class="card mb-3 cardEvent" data-id="${i}" style="cursor: pointer;flex-grow: 1;">
+            <div class="d-flex">
+                <div class="card-body">
+                    ${t.nomTask}
+                </div>
+            </div>
+        </div>`;
+    });
+
+    $('#sectionEvent').html(htmlTachesEvent);
+}
+
+function showRapport() {
+    let rapportTasks = getRapportTask();
+    let htmlTachesRapport = ``;
+
+    rapportTasks.forEach(function (task) {
+        if (!task.statutTask) {
+            let i = getIndexTask(task);
+
+            htmlTachesRapport += `
+                <div class="card mb-3 cardEvent" data-id="${i}" style="cursor: pointer;flex-grow: 1;">
+                    <div class="d-flex">
+                        <div class="card-body">
+                            ${task.nomTask}
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    });
+
+    $('#sectionRapport').html(htmlTachesRapport);
+}
+
+function showImportantFromOtherPerso() {
+    let persos = getPersoFromGroupeInactif();
+    let htmlOtherTask = '';
+    console.log(persos);
+
+    persos.forEach(function (p) {
+        let tasks = getTachesImportanteFromPerso(p);
+
+        tasks.sort((a, b) => {
+            return a.prioTask - b.prioTask;
+        });
+
+        tasks.forEach(function (t) {
+            let i = getIndexTask(t);
+
+            htmlOtherTask += `
+            <div class="card mb-3 cardEvent" data-id="${i}" style="cursor: pointer;flex-grow: 1;">
+                <div class="d-flex">
+                    <div class="card-body">
+                        ${t.nomTask}<br>
+                        <i class="color-gray">${t.persoTask}</i>
+                    </div>
+                </div>
+            </div>`;
+        });
+    });
+
+    $('#sectionOtherPersos').html(htmlOtherTask);
+}
+
+function showCounter() {
+    $('#counterUna').html(db.get('counterUna').value());
+    $('#counterChaos').html(db.get('counterChaos').value());
+    $('#counterRaid').html(db.get('counterRaid').value());
+}
 
 function getPersoFromGroupe(g) {
     db.get("personnages").value().forEach(function (p) {
@@ -49,11 +179,35 @@ function getPersoFromGroupe(g) {
     });
 }
 
-function getTachesFromPerso(p) {
+function getPersoFromGroupeInactif() {
+    let persos = [];
+    
+    db.get("personnages").value().forEach(function (p) {
+        if (p.groupePerso != 'Main' && p.groupePerso != db.get("groupeEnCours").value()) {
+            persos.push(p);
+        }
+    });
+
+    return persos;
+}
+
+function getTachesActiveFromPerso(p) {
     let tasks = [];
 
     db.get("tasks").value().forEach(function (t) {
-        if (t.persoTask == p.typePerso) {
+        if (t.persoTask == p.typePerso && !t.statutTask && t.typeTask != 'Rapport') {
+            tasks.push(t);
+        }
+    });
+
+    return tasks;
+}
+
+function getTachesImportanteFromPerso(p) {
+    let tasks = [];
+
+    db.get("tasks").value().forEach(function (t) {
+        if (t.persoTask == p.typePerso && !t.statutTask && (t.typeTask == 'Guild Activities' || t.typeTask == 'Daily Una Task' || t.typeTask == 'Récupération Weekly Una Task')) {
             tasks.push(t);
         }
     });
@@ -69,4 +223,76 @@ function getIndexTask(task) {
     });
 
     return index;
+}
+
+function getColorFromTask(task) {
+    switch (task.resetTask) {
+        case 'Daily':
+            return 'taskRed';
+        case 'Weekly':
+            return 'taskBlue';
+        default:
+            return 'taskGray';
+    }
+}
+
+function getEventTask() {
+    let eventTasks = [];
+
+    db.get("tasks").value().forEach(function (task) {
+        if (task.openingTask && task.openingTask.length > 0) eventTasks.push(task);
+    });
+
+    return eventTasks;
+}
+
+function getRapportTask() {
+    let rapportTasks = [];
+
+    db.get("tasks").value().forEach(function (task) {
+        if (task.typeTask == 'Rapport') rapportTasks.push(task);
+    });
+
+    return rapportTasks;
+}
+
+function getTasksEventDaily() {
+    let tasks = [];
+
+    db.get("tasks").value().forEach(function (t, i) {
+        if (task == t) index = i;
+    });
+
+    return tasks
+}
+
+function checkTask(index) {
+
+    if (db.get('tasks').get(index).get('resetTask').value() == 'Unique') {
+        db.get("tasks").get(index).delete(true);
+        db.save();
+    } else {
+        db.get("tasks")
+            .get(index)
+            .get('statutTask')
+            .set(true);
+        db.save();
+
+        let task = db.get("tasks").get(index).value();
+
+        if (task.typeTask == 'Daily Una Task') {
+            incrementeCounter('counterUna');
+        } else if (task.typeTask == 'Chaos Dungeon') {
+            incrementeCounter('counterChaos');
+        } else if (task.typeTask == 'Guardian Raid') {
+            incrementeCounter('counterRaid');
+        }
+    }
+}
+
+function incrementeCounter(counter) {
+    db.get(counter).set(parseInt(db.get(counter).value()) + 1);
+    db.save();
+
+    showCounter();
 }
